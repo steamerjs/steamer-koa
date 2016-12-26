818bdde823f0aa884090b2e1b2800f57efa71690
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2016-12-26 18:50:48 +0800

    change structure

---
+++ .steamer/steamer-koa.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "files": [
+        "src",
+        "tools",
+        "README.md"
+    ],
+    options: [
+        {
+            type: 'input',
+            name: 'host',
+            message: 'server host(steamer.com)',
+            default: "steamer.com",
+        },
+        {
+            type: 'input',
+            name: 'mongo-server',
+            message: 'mongo server(127.0.0.1)',
+            default: "127.0.0.1",
+        },
+        {
+            type: 'input',
+            name: 'mongo-port',
+            message: 'mongo server port(27017)'
+        },
+        {
+            type: 'input',
+            name: 'mongo-db',
+            message: 'mongo server db(steamer)',
+            default: 'steamer'
+        }
+    ]
+};
